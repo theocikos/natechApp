bb03fdf36b644622fcb708f46871cd13ee3e67b1
Theodosis Koutsias <theoc44@hotmail.com>
2025-06-09 14:43:03 +0300

    feat: Wrap RootLayout in SessionProvider and add StatusBar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,22 @@
+import { SessionProvider } from "@/nucleus";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(without-session)" options={{ animation: "none" }} />
-      <Stack.Screen name="(with-session)" options={{ animation: "none" }} />
-      <Stack.Screen name="index" options={{ animation: "none" }} />
-    </Stack>
+    <>
+      <StatusBar />
+      <SessionProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="(without-session)"
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen name="(with-session)" options={{ animation: "none" }} />
+          <Stack.Screen name="index" options={{ animation: "none" }} />
+        </Stack>
+      </SessionProvider>
+    </>
   );
 }
