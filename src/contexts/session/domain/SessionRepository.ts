9b441772a32e7b0f284c96ebc32e5f2d1180675f
Theodosis Koutsias <theoc44@hotmail.com>
2025-06-10 00:35:43 +0300

    feat: Add getSession method to SessionRepository interface

--- src/contexts/session/domain/SessionRepository.ts
+++ src/contexts/session/domain/SessionRepository.ts
@@ -3,4 +3,5 @@ import { Session } from "./Session";
 export interface SessionRepository {
   signInWithPassword(email: string, password: string): Promise<Session>;
   logout(sessionId: string): Promise<void>;
+  getSession(): Promise<Session | null>;
 }
