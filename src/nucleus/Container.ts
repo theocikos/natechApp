36b3741c2d78db2df911e12f44db5f6643d99476
Theodosis Koutsias <theoc44@hotmail.com>
2025-06-10 00:37:02 +0300

    chore: Add GetSession to NTContainer for session management

--- src/nucleus/Container.ts
+++ src/nucleus/Container.ts
@@ -1,4 +1,4 @@
-import { CreateSessionWithPassword, EndSession } from "@/contexts/session/application";
+import { CreateSessionWithPassword, EndSession, GetSession } from "@/contexts/session/application";
 import { FetchSessionRepository } from "@/contexts/session/infrastructure";
 import { AsyncStorageService } from "@/contexts/shared/infrastructure";
 
@@ -12,6 +12,7 @@ class NTContainer {
   private sessionRepository: FetchSessionRepository;
   private _createSessionWithPassword: CreateSessionWithPassword;
   private _endSession: EndSession;
+  private _getSession: GetSession;
 
   private constructor() {
     this.storageService = new AsyncStorageService();
@@ -21,6 +22,7 @@ class NTContainer {
       this.sessionRepository
     );
     this._endSession = new EndSession(this.sessionRepository);
+    this._getSession = new GetSession(this.sessionRepository);
   }
 
   public static getInstance(): NTContainer {
@@ -36,6 +38,9 @@ class NTContainer {
   }
   get endSession() {
     return this._endSession;
+  }
+  get getSession() {
+    return this._getSession;
   }
 }
 
