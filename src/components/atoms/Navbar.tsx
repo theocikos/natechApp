ef2fb324e5570263c4c45edfed4b444ea50b9090
Theodosis Koutsias <theoc44@hotmail.com>
2025-06-10 15:18:28 +0300

    feat: Add NavBar component for navigation functionality

---
+++ src/components/atoms/Navbar.tsx
@@ -0,0 +1,60 @@
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useRouter } from "expo-router";
+import { FC, useCallback } from "react";
+import {
+    GestureResponderEvent,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+
+import { AppRoutes } from "@/enums/misc";
+import { NTText, NTView } from "../native";
+
+export type NavBarProps = {
+  onPress?: (event: GestureResponderEvent) => void;
+  text: string;
+};
+
+const NavBar: FC<NavBarProps> = ({ text, onPress }) => {
+  const router = useRouter();
+
+  const handlePress = useCallback(
+    (event: GestureResponderEvent) => {
+      onPress?.(event);
+      if (router.canGoBack()) return router.back();
+      if (router.canDismiss()) return router.dismiss();
+      router.replace(AppRoutes.INDEX.build());
+    },
+    [router, onPress]
+  );
+
+  return (
+    <NTView style={styles.header}>
+      <TouchableOpacity
+        onPress={handlePress}
+        accessibilityLabel="Go back"
+        accessibilityRole="button"
+      >
+        <MaterialCommunityIcons name="arrow-left" size={24} />
+      </TouchableOpacity>
+      <NTText style={styles.headerTitle}>{text}</NTText>
+    </NTView>
+  );
+};
+
+NavBar.displayName = "NavBar";
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+    marginBottom: 16,
+  },
+  headerTitle: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+});
+
+export { NavBar };
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,5 @@
 export * from './Button';
 export * from './Image';
 export * from './Input';
+export * from './Navbar';
 
