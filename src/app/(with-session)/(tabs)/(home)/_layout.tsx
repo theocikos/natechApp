74b4ea6c3b050ac2efb7658482cf5686fbca847b
Theodosis Koutsias <theoc44@hotmail.com>
2025-06-10 20:37:44 +0300

    feat: Add additional screens to ExpoRouterHomeLayout

--- src/app/(with-session)/(tabs)/(home)/_layout.tsx
+++ src/app/(with-session)/(tabs)/(home)/_layout.tsx
@@ -4,6 +4,9 @@ export default function ExpoRouterHomeLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+      <Stack.Screen name="send-money" />
+      <Stack.Screen name="review-transaction" />
+      <Stack.Screen name="feedback" />
     </Stack>
   );
 }
